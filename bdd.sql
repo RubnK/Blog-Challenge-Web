264390cdd7844e755cbca039f5c90722046ab434
RubnK <40402492+RubnK@users.noreply.github.com>
2025-01-08 10:38:42 +0100

    Création de la base de données

---
+++ bdd.sql
@@ -0,0 +1,49 @@
+-- First, you have to create a database called "blog" in your PostgreSQL server
+
+-- Create the users table
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_admin BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE categories (
+    category_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
+);
+
+-- Create the articles table
+CREATE TABLE articles (
+    article_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    category_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    shares INT DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
+-- Create the comments table
+CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    article_id INT REFERENCES articles(article_id),
+    user_id INT REFERENCES users(user_id),
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (article_id) REFERENCES articles(article_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- Create the likes table
+CREATE TABLE likes (
+    like_id SERIAL PRIMARY KEY,
+    article_id INT REFERENCES articles(article_id),
+    user_id INT REFERENCES users(user_id),
+    FOREIGN KEY (article_id) REFERENCES articles(article_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
